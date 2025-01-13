0b4f47371ed59370959e7c593dab9502d3e3ce00
vishal kumar verma <vv081997@gmail.com>
2025-01-13 16:05:06 +0530

    Add components for sales, product, customer, supplier, inventory, and authentication with basic templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from "./header/header.component";
+import { SideNavbarComponent } from "./side-navbar/side-navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, HeaderComponent, SideNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Medical_managment';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ProductComponent } from './product/product.component';
+import { SalesComponent } from './sales/sales.component';
+import { SupplierComponent } from './supplier/supplier.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {path: '', redirectTo: '/home', pathMatch: 'full'},
+  // {path: 'home', component:AppComponent},
+  {path: 'customer', component:CustomerComponent},
+  {path: 'inventory', component:InventoryComponent},
+  {path: 'product', component:ProductComponent},
+  {path: 'sales', component:SalesComponent},
+  {path: 'supplier', component:SupplierComponent},
+  {path: 'auth', component:AuthenticationComponent},
+
+];
